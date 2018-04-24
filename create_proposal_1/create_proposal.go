3d7d4869c52b7f43109bce66b4f859bedbd57e2f
abhi-005 <31097549+abhi-005@users.noreply.github.com>
2018-04-24 11:34:23 +0530

    Create create_proposal.go

---
+++ create_proposal_1/create_proposal.go
@@ -0,0 +1,14 @@
+/***** Project ChainDaaS - Phase0 - SRC - UI1 for Creation Purchase Order ***/
+/*** Last updated on 29th Nov 2017  by Santhosh Balaji ***/
+
+
+package main
+
+import (     
+"errors"
+"fmt"
+"strconv"
+"encoding/json"
+"github.com/hyperledger/fabric/core/chaincode/shim"
+//"github.com/hyperledger/fabric/core/util"
+)
